b0a46d6515eb5109716541403ca596c70485b254
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-11-21 16:49:04 -0300

    Estrutura de um Nó de uma Árvore Binária

---
+++ 43. estrutura_no.c
@@ -0,0 +1,15 @@
+struct No {
+    char valor[50];
+    struct No* esquerda;
+    struct No* direita;
+};
+
+// Essa estrutura define um nó que armazena uma string e dois ponteiros para os filhos esquerdo e direito. 
+// Cada instância de struct No é um elemento da árvore. Lembre-se: essa é a base para todas as operações com árvores binárias.
+
+// A estrutura correta do nó viabiliza a construção de árvores de qualquer forma, conectando nós filhos 
+// de maneira livre e permitindo chamadas recursivas para percorrer ou modificar a árvore.
+
+
+
+
