edcd4b8f61447d093d45bd9b793e530afcccb377
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-11-26 14:47:30 -0300

    Introdução ao Estudo sobre Árvores Trie

---
+++ 60. introducao_arvore_trie.c
@@ -0,0 +1,10 @@
+// Introdução a árvores Trie
+
+// Quando falamos de árvore Trie, precisamos saber que a pronúncia correta é “trai” e que é também conhecida como 
+//árvore de prefixos. Ela é uma estrutura de dados especializada no armazenamento e manipulação de strings, com 
+// excelente desempenho para buscas rápidas, autocompletar e verificação de palavras. Ao contrário das árvores BST, 
+// que organizam os dados de acordo com valores numéricos ou ordem alfabética total, a Trie organiza cada 
+// palavra letra por letra, aproveitando ao máximo os prefixos comuns entre elas.
+
+// Essa estrutura é bastante utilizada em dicionários digitais, sistemas de correção automática, algoritmos de 
+// compressão e qualquer aplicação que envolva grande quantidade de palavras ou buscas frequentes com base em prefixos.
